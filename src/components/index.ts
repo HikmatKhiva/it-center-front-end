79dc8d6d1c0d2e851ff24bfa9e27bbe874544b1b
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2023-06-04 08:36:59 -0400

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,6 @@
 export { default as Team } from "./Team";
-export { default as CourseCard } from "./CourseCard";
 export { default as Application } from "./Application";
 export { default as Complaint } from "./Complaint";
 export { default as NavItem } from "./NavItem";
 export { default as Social } from "./Social";
-
 export { default as Map } from "./Map"
