c4b17a6a6628274b975116dec761ce5b07567a4a
HikmatKhiva <xikmatkhiva.4s@gmail.com>
2023-06-04 08:25:05 -0400

    updated

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -1,31 +1,16 @@
 import { useEffect } from "react";
-import { CourseCard } from "../components";
-import useGetData from "../hooks/useGetData";
-import { courseQ } from "../server/query";
-import { ICourse } from "../types/types";
-import { Error, FetchLoading, EmptyPage } from "../loadings";
+import { Remount } from "../loadings";
 const Course = () => {
   useEffect(() => {
     document.title = "Kurslar";
   }, []);
-  const { data, loading, error } = useGetData(courseQ);
   return (
     <section id="course" className="dark:bg-dark bg-gray-100 flex flex-col">
       <div className="title py-5 dark:text-main flex items-center flex-col">
         <h2 className="text-3xl capitalize">Kurslar</h2>
       </div>
       <div className="container mx-auto flex justify-center flex-wrap flex-grow gap-5">
-        {data && !loading && !error ? (
-          data?.map((course: ICourse, index: number) => (
-            <CourseCard key={course.name} item={course} index={index} />
-          ))
-        ) : error ? (
-          <Error error={error} />
-        ) : data?.length === 0 ? (
-          <EmptyPage />
-        ) : (
-          <FetchLoading />
-        )}
+       <Remount />
       </div>
     </section>
   );
