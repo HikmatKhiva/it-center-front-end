bb278aa61abb143ea24ecea8aa8c5a545296d06a
HikmatKhiva <xikmatkhiva.4s@gmail.com>
2023-06-20 16:42:20 +0500

    update add react-query for fetch data

--- src/components/LatestNews.tsx
+++ src/components/LatestNews.tsx
@@ -2,11 +2,11 @@ import { motion } from "framer-motion";
 import { fadeTitle } from "../utils/motion";
 import NewsCard from "./NewsCard";
 import { container } from "../utils/motion";
-import useGetData from "../hooks/useGetData";
 import { news } from "../server/query";
 import { INews } from "../types/types";
+import { useQueryData } from "../hooks/useQueryData";
 const LatestNews = () => {
-  const { data } = useGetData(news);
+  const { data } = useQueryData(news);
   return (
     <section className="latest-news bg-gray-100 py-10 dark:bg-dark">
       <div className="title flex flex-col items-center">
