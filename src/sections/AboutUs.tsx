002def1ed070b1336cc254ecbae42148efc9abf7
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2023-07-06 22:52:53 +0500

    Update AboutUs.tsx

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -2,11 +2,11 @@ import { Tilt } from "react-tilt";
 import { Logo } from "../animationElement";
 import { itCenter } from "../assets";
 import { Link } from "react-router-dom";
-import { logo } from "../settings";
+import { logo,defaultOptions } from "../settings";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 const AboutUs = () => {
   return (
-    <section id="about-us" className="py-5 bg-gray-100 overflow-x-hiddenT dark:bg-dark">
+    <section id="about-us" className="py-5 bg-gray-100 overflow-x-hidden dark:bg-dark">
       <div className="container mx-auto">
         <div className="title py-5 dark:text-white flex items-center flex-col">
           <h2 className="text-3xl capitalize">Biz haqimizda</h2>
@@ -30,7 +30,7 @@ const AboutUs = () => {
               batafsil o'qish
             </Link>
           </div>
-          <Tilt op>
+          <Tilt options={defaultOptions}>
             <LazyLoadImage
               effect="blur"
               className="xl:w-[500px] md:w-[400px] w-full object-cover lg:rounded-tr-full rounded-xl  max-h-[350px] rounded-br-2xl lg:rounded-br-none lg:rounded-bl-full"
