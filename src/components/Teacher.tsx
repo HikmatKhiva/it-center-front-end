9c1b5cb91490bfb286a1f2bf592381deac56b7d0
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2023-08-18 22:30:17 +0500

    Update Teacher.tsx

--- src/components/Teacher.tsx
+++ src/components/Teacher.tsx
@@ -6,28 +6,37 @@ import { ISocial, ITeam } from "../types/types";
 import Social from "./Social";
 import { urlFor } from "../server/client";
 import { LazyLoadImage } from "react-lazy-load-image-component";
-const Teacher = ({ index, teacher }: { index: number, teacher: ITeam }) => {
+const Teacher = ({ index, teacher }: { index: number; teacher: ITeam }) => {
   return (
-    <motion.div whileInView={fadeCardAnimate(index * .5, 1)} className="dark:text-white team py-5 shadow-card-light dark:shadow-card-dark bg-white w-[350px] rounded-md overflow-hidden dark:bg-light-dark">
+    <motion.div
+      whileInView={fadeCardAnimate(index * 0.5, 1)}
+      className="dark:text-white team py-5 shadow-card-light dark:shadow-card-dark bg-white w-[350px] rounded-md overflow-hidden dark:bg-light-dark"
+    >
       <Tilt options={defaultOptions}>
         <div className="flex flex-col items-center">
           <LazyLoadImage
             className="w-32 rounded-full bg-transparent "
             effect="blur"
             src={urlFor(teacher.image).url()}
-            alt={teacher.name} />
+            alt={teacher.name}
+          />
           <div className="py-3 px-4 flex flex-col">
-            <h4 className="text-2xl text-center text-main">{teacher?.occupation}</h4>
+            <h4 className="text-2xl text-center text-main">
+              {teacher?.occupation}
+            </h4>
             <div className="text-center">
               <h5 className="font-medium">{teacher.name}</h5>
               <div className="flex justify-center pt-2 gap-3">
-                {teacher?.stacks.map((social: ISocial, index: number) => <Social key={index} social={social} index={index} />)}
+                {teacher.stacks &&
+                  teacher?.stacks.map((social: ISocial, index: number) => (
+                    <Social key={index} social={social} index={index} />
+                  ))}
               </div>
             </div>
           </div>
         </div>
       </Tilt>
     </motion.div>
-  )
-}
+  );
+};
 export default Teacher;
