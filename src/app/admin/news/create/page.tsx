71f6d83090869b1649e4a4bbe90f455d01f98e55
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2024-06-16 22:46:56 +0500

    Update page.tsx

--- src/app/admin/news/create/page.tsx
+++ src/app/admin/news/create/page.tsx
@@ -1,75 +1,153 @@
 "use client";
-import React, { ChangeEvent, Suspense, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import BackButton from "../../components/BackButton";
 import { Input } from "@/components/ui/input";
-import RichTextEditor from "../components/RichTextEditor";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import Image from "next/image";
-
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormField,
+  FormControl,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { newsFormSchema } from "../../utils/validation/newsFormSchema";
+import { z } from "zod";
+import { HoverBorder } from "@/components/ui/hoverBorder";
+import { modules } from "../../utils/richTextModule";
+import { Button } from "@nextui-org/button";
 const AdminNewsCreatePage = () => {
-  const [news, setNews] = useState<INewsState>({
-    title: "",
-    description: "",
-    image: null,
+  const [image, setImage] = useState<null | File>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const form = useForm<z.output<typeof newsFormSchema>>({
+    resolver: zodResolver(newsFormSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      content: "",
+    },
   });
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.name === "image" && event.target?.files) {
-      const image = event.target.files[0];
-      setNews((prev) => ({ ...prev, image }));
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setImage(event.target.files[0]);
     }
-    setNews((prev) => ({ ...prev, [event.target.name]: event.target.value }));
   };
-  console.log(news);
-
+  const onSubmit = async (data: z.output<typeof newsFormSchema>) => {
+    const formData = new FormData();
+    if (image) {
+      formData.append("image", image);
+    }
+    formData.append("title", data.title);
+    formData.append("description", data.description);
+    formData.append("content", data.content);
+  };
   return (
     <div>
-      <BackButton />
-      <h2 className="text-2xl text-center">Yangilik Yaratish</h2>
-      <form className="w-[90%] mx-auto mt-5 flex flex-col gap-3">
-        <div className="flex gap-5">
-          <div className="flex-grow flex flex-col justify-between gap-2">
-            <Input
-              name="title"
-              onChange={handleChange}
-              className="dark:bg-background/70"
-              placeholder="Yangilik sarlavhasini kiriting"
-            />
-            <Input
-              onChange={handleChange}
-              name="description"
-              className="dark:bg-background/70"
-              placeholder="Yangilik uchun qiqa ma'lumot kiriting"
-            />
-          </div>
-          <label
-            htmlFor="image"
-            className="w-[100px] h-[100px] text-center rounded border flex justify-center items-center dark:bg-background/70"
-          >
-            {!news.image ? (
-              <span>
-                Upload <br /> Image
-              </span>
-            ) : (
-              <Image
-                src={news.image}
-                alt="upload image"
-                width={100}
-                height={100}
+      <div className="flex justify-between items-center">
+        <BackButton />
+        <h2 className="text-2xl text-center">Yangilik Yaratish</h2>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-[90%] mx-auto mt-5 flex flex-col gap-3"
+        >
+          <div className="flex flex-col justify-center items-center ">
+            <label
+              htmlFor="image"
+              className="w-[200px] h-[200px] mx-auto text-center flex flex-col justify-center items-center rounded border dark:bg-background/70"
+            >
+              {!image ? (
+                <span>
+                  Surat <br /> yuklash
+                </span>
+              ) : (
+                <>
+                  <Image
+                    src={URL.createObjectURL(image)}
+                    alt="upload image"
+                    width={150}
+                    height={150}
+                  />{" "}
+                  <br />
+                </>
+              )}
+
+              <Input
+                id="image"
+                onChange={handleFileChange}
+                name="image"
+                accept=".png,.jpg"
+                className="dark:bg-background/70 hidden"
+                type="file"
+                placeholder="Yangilik uchun qiqa ma'lumot kiriting"
               />
+            </label>
+            {image && <span className="font-semibold">{image.name}</span>}
+          </div>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="dark:bg-background/70"
+                    placeholder="Yangilik sarlavhasini kiriting"
+                  />
+                </FormControl>
+                <FormMessage className="text-red-500" />
+              </FormItem>
             )}
-            <Input
-              id="image"
-              onChange={handleChange}
-              name="image"
-              className="dark:bg-background/70 hidden"
-              type="file"
-              placeholder="Yangilik uchun qiqa ma'lumot kiriting"
-            />
-          </label>
-        </div>
-        <Suspense fallback="Loading">
-          <RichTextEditor />
-        </Suspense>
-      </form>
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    {...field}
+                    className="dark:bg-background/70"
+                    placeholder="Yangilik sarlavhasini kiriting"
+                  />
+                </FormControl>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <HoverBorder>
+                    <ReactQuill
+                      {...field}
+                      className="min-h-20 dark:bg-background/70 border-black"
+                      modules={modules}
+                      theme="snow"
+                    />
+                  </HoverBorder>
+                </FormControl>
+                <FormMessage className="text-red-500" />
+              </FormItem>
+            )}
+          />
+          <Button
+            radius="sm"
+            className="bg-green-500 hover:bg-green-600"
+            type="submit"
+          >
+            Saqlash
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
