e223013e37a904d67b9307b0930d192dbdb8f5f0
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2024-06-16 22:47:00 +0500

    Create richTextModule.ts

---
+++ src/app/admin/utils/richTextModule.ts
@@ -0,0 +1,15 @@
+export const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    ["image", "code-block", "link"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    [{ color: [] }, { background: [] }],
+    [{ align: [] }],
+  ],
+};
