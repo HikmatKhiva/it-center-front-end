43efb92ea91dd8cbf60931491092973312fdef37
Hikmat <xikmatkhiva.4s@gmail.com>
2024-06-17 12:24:18 +0500

    Update HeroSection.tsx

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,11 +1,19 @@
+import { FollowerPointerCard } from "@/components/ui/following-pointer";
+import { TextRevealCard } from "@/components/ui/text-reveal-card";
 const HeroSection = () => {
   return (
     <div className="container relative mx-auto py-10 z-20">
-      <h1 className="md:text-7xl text-2xl font-bold dark:text-white">
-        Sizning <span className="text-main">IT</span> karyerangizni <br /> shu
-        yerdan boshlang!
-      </h1>
-      {/* <p>Bizning o'quv markazimizda zamonayiv kasblarni mutaxassislar tomonidan o`gatiladi</p> */}
+      <FollowerPointerCard>
+        <h1 className="md:text-7xl text-2xl font-bold dark:text-white">
+          Sizning <span className="text-main">IT</span> karyerangizni <br /> shu
+          yerdan boshlang!
+        </h1>
+        <TextRevealCard
+          text="You know the business"
+          revealText="I know the chemistry"
+          className=""
+        />
+      </FollowerPointerCard>
     </div>
   );
 };
