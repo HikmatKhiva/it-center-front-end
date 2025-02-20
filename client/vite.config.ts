e637f1d9a764cdba1bc3213ee6c490ecbf75b5d9
Hikmat <94689644+HikmatKhiva@users.noreply.github.com>
2025-02-20 22:26:54 +0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
 });
